1edb00a2f4fdc41a33cd53c5d3d393e3daeb37ec
Cristiananc <cristiananc@hotmail.com>
2022-10-03 15:46:04 -0300

    creating a script for simulating all steps at once

---
+++ R/simulations_step_by_step.R
@@ -0,0 +1,26 @@
+source("R/01-simulation_sampling_genealogies.R")
+source("R/02-simulations_reconstructing_effepop.R")
+source("R/03-simulations_calculating_performance_metrics.R")
+
+simulation_step_by_step <- function(path, n_sim, date_min, date_max, traj,
+                                    n_taxa, name_traj){
+  
+  stimes <- sort(runif(n_taxa, date_min, date_max))
+  
+  #FIRST STEP #####
+  #Here we simulate 500 different genealogies with 20 taxa
+  outpath_01 <- paste0(path, "genealogies_", name_traj, "_", n_taxa, "taxa.rdata")
+  sim_sampling_genealogies(n_sim, stimes, date_min, date_max, n_taxa, traj, outpath_01) 
+  
+  #SECOND STEP ###
+  #The next step is the phylodynamic reconstruction of the effective population size for the
+  #trees with 20 taxa for each precision prior analysed
+  outpath_02 <- paste0(path, name_traj, "_", n_taxa, "taxa_")
+  a <- sample(1:n_sim, 1)
+  sim_reconstructing_effepop(outpath_01, outpath_02, traj, n_taxa, n_sim)
+  
+  #THIRD STEP ##########
+  #Now we calculate the performance metrics
+  R <- stimes[length(stimes)]
+  sim_calculating_performance_metrics(traj, R, date_max, n_sim, outpath_02)
+}
