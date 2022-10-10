dca7108d24b10ab975ef21bb611a2746cce04d0a
Cristiananc <cristiananc@hotmail.com>
2022-10-10 13:37:26 -0300

    fixing spelling error

--- R/simulation_cyclical.R
+++ R/simulation_cyclical.R
@@ -2,14 +2,14 @@ source("R/simulations_step_by_step.R")
 set.seed(29)
 
 #Setting input variables
-path <- "./data/simulations/cycllical/"
-path_fig <- "./figs/simulations/cycllical/"
+path <- "./data/simulations/cyclical/"
+path_fig <- "./figs/simulations/cyclical/"
 n_sim <- 500
 date_min <- 0
-date_max <- 31
+date_max <- 31  
 
 #The real trajectory of the phylogenies simulated
-name_traj <- "cycllical_traj1"
+name_traj <- "cyclical_traj1"
 trajectory_cyclic <- cyclic_traj
 
 plot(trajectory_cyclic(seq(0, 31)), type = 'l', ylab = "Effective population size", 
@@ -19,10 +19,27 @@ plot(trajectory_cyclic(seq(0, 31)), type = 'l', ylab = "Effective population siz
 n_taxa <- 20
 
 #Performing simulations:
-simulation_step_by_step(path, n_sim, date_min, date_max, trajectory_cycllical, 
+simulation_step_by_step(path, n_sim, date_min, date_max, trajectory_cyclic, 
                         n_taxa, name_traj, path_fig)
 
 #SIMULATION WITH 200 TAXA ####
 n_taxa <- 200
-simulation_step_by_step(path, n_sim, date_min, date_max, trajectory_cycllical, 
+simulation_step_by_step(path, n_sim, date_min, date_max, trajectory_cyclic, 
                         n_taxa, name_traj, path_fig)
+
+#Plotting log of effective population size for both 20 and 200 taxa
+#"cyclical_traj1_4taxa_reconstructions_pc_prior_summary"
+sgamma20 <- list.load()
+sgamma200 <- list.load()
+
+s_mgamma20 <- list.load()
+s_mgamma200 <- list.load()
+
+spcprior20 <- list.load()
+spcprior200 <- list.load()
+
+all_estimates <- rbind(sgamma20, sgamma200, s_mgamma20, s_mgamma200, spcprior20, spcprior200)
+all_estimates$ntaxa <- as.factor(all_estimates$ntaxa)
+all_estimates$replicate <- as.factor(all_estimates$replicate)
+
+plot_log_effepop(all_estimates, trajectory_cyclic)
