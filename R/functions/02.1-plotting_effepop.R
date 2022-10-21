177d89c54208ea2d8ae5d3ecdfca9cfbc8686b76
Cristiananc <cristiananc@hotmail.com>
2022-10-21 14:16:34 -0300

    ploting effepop for both cases with 20 and 200 taxa

--- R/functions/02.1-plotting_effepop.R
+++ R/functions/02.1-plotting_effepop.R
@@ -24,8 +24,26 @@ plot_log_effepop <- function(effepop, trajectory){
     facet_grid(ntaxa~prior, scales = "free_y") +
     guides(col = "none") +
     theme_bw() +
-    stat_function(fun = trajectory, colour = 'black') +
-    NULL
+    stat_function(fun = trajectory, colour = 'black')
   
-  return(peffpop)
+  show(peffpop)
+}
+
+plot_log_effepop_20_and_200_taxa <- function(summary_path, path_fig, traj){
+  file_ext <- "_summary.rdata"
+  sgamma20 <- list.load(paste0(summary_path, "_20taxa_reconstructions_gamma", file_ext))
+  s_mgamma20 <- list.load(paste0(summary_path,"_20taxa_reconstructions_matching_gamma", file_ext))
+  spcprior20 <- list.load(paste0(summary_path, "_20taxa_reconstructions_pc_prior", file_ext))
+  
+  sgamma200 <- list.load(paste0(summary_path, "_200taxa_reconstructions_gamma", file_ext))
+  s_mgamma200 <- list.load(paste0(summary_path,"_200taxa_reconstructions_matching_gamma", file_ext))
+  spcprior200 <- list.load(paste0(summary_path, "_200taxa_reconstructions_pc_prior", file_ext))
+  
+  all_estimates <- rbind(sgamma20, sgamma200, s_mgamma20, s_mgamma200, spcprior20, spcprior200)
+  all_estimates$ntaxa <- as.factor(all_estimates$ntaxa)
+  all_estimates$replicate <- as.factor(all_estimates$replicate)
+  
+  plot_log_effepop(all_estimates, traj)
+  dev.copy2pdf(file = paste0(path_fig, "plot_log_effepop_size.pdf"), 
+             width = 12, height = 8, out.type = "pdf")
 }
